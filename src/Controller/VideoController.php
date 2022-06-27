3e25e9acb2325bfb113f4bb9ddf687162e13f31f
VladislavLemesh <90740323+VladislavLemesh@users.noreply.github.com>
2022-06-27 16:24:32 +1000

    Add files via upload

--- src/Controller/VideoController.php
+++ src/Controller/VideoController.php
@@ -17,8 +17,7 @@ class VideoController extends AbstractController
     #[Route('/show_videos', name: 'show_videos')]
     public function showVideos(ManagerRegistry $doctrine): Response
     {
-        //$videos = $doctrine->getRepository(Video::class)->findAll();
-        $videos = $doctrine->getRepository(Video::class)->findAllWithCategories();
+        $videos = $doctrine->getRepository(Video::class)->findAll();
         return $this->render('video/show_videos.html.twig', [
           'videos' => $videos
         ]);
@@ -51,4 +50,32 @@ class VideoController extends AbstractController
       'videos' => $videos
     ]);
   }
+
+    #[Route('/{id}/edit', name: 'app_video_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Video $video, VideoRepository $videoRepository): Response
+    {
+        $form = $this->createForm(VideoType::class, $video);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $videoRepository->add($video, true);
+
+            return $this->redirectToRoute('show_videos', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('video/edit.html.twig', [
+            'video' => $video,
+            'form' => $form,
+        ]);
+    }
+    
+    #[Route('/{id}', name: 'app_video_delete', methods: ['POST'])]
+    public function delete(Request $request, Video $video, VideoRepository $videoRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$video->getId(), $request->request->get('_token'))) {
+            $videoRepository->remove($video, true);
+        }
+
+        return $this->redirectToRoute('show_videos', [], Response::HTTP_SEE_OTHER);
+    }
 }
